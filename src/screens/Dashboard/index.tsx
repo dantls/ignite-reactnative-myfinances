366b41547bdaafa09287e90a7430766b8f281084
dantls <dan_tls@hotmail.com>
2021-05-27 23:50:07 -0400

    Add Barcode

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
 import {Container, Title} from './styles';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export function Dashboard(){
+  const [hasPermission, setHasPermission] = useState(false);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+ 
+  interface dataProps{ 
+    type: BarCodeScanner.Constants.BarCodeType;
+    data: string
+  }
+  
+
+  const handleBarCodeScanned = ({ type, data}:dataProps) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return(
     <Container>
-      <Title>Dashboard</Title>
+    <View >
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  
+
     </Container>
   )
 }
